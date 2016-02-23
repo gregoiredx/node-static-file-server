c72dbfd1a6b7122fc6b0c976d530eb6d56962cf1
Grégoire Deveaux <ggdx@laposte.net>
2016-02-23 22:27:46 +0100

    configurable port

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 var express = require('express');
 
 var static_files_dir = process.argv.length > 2 ? process.argv[2] : __dirname + '/public';
+var port = process.argv.length > 3 ? process.argv[3] : 10001;
  
 var server = express();
 server.use(express.static(static_files_dir));
  
-var port = 10001;
 server.listen(port, function() {
-    console.log('server listening on port ' + port);
+    console.log('serving files from ' + static_files_dir + ' on port ' + port);
 });
